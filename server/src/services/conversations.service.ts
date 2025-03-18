1edbda7cf2684d938c493d486c1b80e8e69b2f9b
unknown <veb7vmehra@gmail.com>
2025-03-18 15:09:52 +0530

    console for bug catch

--- server/src/services/conversations.service.ts
+++ server/src/services/conversations.service.ts
@@ -217,6 +217,7 @@ class ConversationsService {
         
         if ( ccr != null && vai != null ) {
             const current_state = await this.getCurrentState(conversationId)
+            console.log(current_state[0]["valence_all"])
             val.push((current_state[0]["valence_all"].reduce((acc, num) => acc + num, 0)) / current_state[0]["count"])
             ar.push((current_state[0]["arousal_all"].reduce((acc, num) => acc+num, 0)) / current_state[0]["count"])
             val.push(Math.max(...current_state[0]["valence_all"]))
